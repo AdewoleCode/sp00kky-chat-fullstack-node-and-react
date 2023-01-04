2c9810801206de3002bac2df4f718a3765fda0ea
adewole <adeadewole1712@gmail.com>
2023-01-04 20:17:50 +0100

    projec done

--- server/app.js
+++ server/app.js
@@ -1,91 +1,86 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-const userRouter = require('./routes/userRoutes')
-const messageRouter = require('./routes/messageRoute')
+const userRouter = require("./routes/userRoutes");
+const messageRouter = require("./routes/messageRoute");
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-const connectDB = require('./DB/connect');
+const connectDB = require("./DB/connect");
 
-const bodyParser = require('body-parser');
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const socket = require("socket.io")
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const socket = require("socket.io");
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(cors())
-
-
-
-
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 //routes
-app.use('/api/auth', userRouter)
-app.use('/api/messages', messageRouter )
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
+// app.get('/', (req, res) => {
+//     res.send('homepage')
+//     console.log("hello");
+// })
 
-app.get('/', (req, res) => {
-    res.send('homepage')
-    console.log("hello");
-})
-
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-
-
-
-
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3030;
 
 //conect to mongodb and start server
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
-    console.log('connected');
-
+    await connectDB(process.env.MONGO_URI);
+    console.log("connected");
   } catch (error) {
     console.log(error);
   }
 };
 
 const server = app.listen(port, () =>
-console.log(`Server is listening on port ${port}...`)
+  console.log(`Server is listening on port ${port}...`)
 );
 
 start();
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
-    credentials: true
-  }
-})
+    origin: "http://localhost:3030",
+    credentials: true,
+  },
+});
 
-global.onlineUsers = new Map()
+global.onlineUsers = new Map();
 
-io.on("connection", (socket)=> {
-  global.chatSocket = socket
-  socket.on("add-user", (userId)=> {
-    onlineUsers.set(userId, socket.id)
-  })
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
 
-  socket.on('send-msg', (data)=> {
-    const sendUserSocket = onlineUsers.get(data.to)
-    if (sendUserSocket){
-      socket.to(sendUserSocket).emit("msg-recieve", data.msg)
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
     }
-  })
-
-})
-
-
-
+  });
+});
--- server/routes/messageRoute.js
+++ server/routes/messageRoute.js
@@ -7,5 +7,13 @@ const { getMsg, createMsg } = require('../controllers/messageController')
 router.route('/getmessage').post(getMsg)
 router.route('/createmessage').post(createMsg)
 
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
+
 
 module.exports = router
