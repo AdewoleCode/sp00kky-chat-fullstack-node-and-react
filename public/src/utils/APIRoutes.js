7a06ac5765e5e51864ee87a1218e71c5fa790d16
adewole <adeadewole1712@gmail.com>
2023-02-13 22:06:41 +0100

    updated api route

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://spooky-chat-api.onrender.com";
+export const host = "https://spooky-api.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
